71cb55d2dc1f1fac92de02b06ee871fb4d868265
XDLOPER <yasirkaygusuzone@hotmail.com>
2022-11-25 23:52:34 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,6 @@ import {myFunc,cubeGenerate,body} from "./main.js";
 
 const button = document.getElementById('button');
 
-button.addEventListener('click',()=>{
-    //myFunc("vuhu!")
-    console.log("cube oluşuyor..")
-    cubeGenerate();
-})
-
 
 setInterval(() => {
     for (let index = 0; index < 1; index++) {
